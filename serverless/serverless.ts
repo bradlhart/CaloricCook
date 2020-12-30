127c12860af0434b15bca3e35ffc1fde0c2f43b8
Brad Hart <brad.l.hart@gmail.com>
2020-12-30 01:21:23 -0500

    Expo and Serverless

---
+++ serverless/serverless.ts
@@ -0,0 +1,39 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'caloriccook',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello',
+          },
+        },
+      ],
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
